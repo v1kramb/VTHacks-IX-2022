4c740c3c62f5fe1143583c1b4bd8fbc20784ca23
Vikram Bala <vikrambala.va@gmail.com>
2022-02-26 00:17:57 -0500

    Added pose detection and WIP pushup classification

---
+++ exercise_classifier/PushUpDetection.py
@@ -0,0 +1,111 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import PoseModule as pm
+import PoseLandmark as pl
+from datetime import datetime
+
+cap = cv2.VideoCapture('short-pushup.mp4')  # 0
+
+# Determine video attributes
+fps = cap.get(cv2.CAP_PROP_FPS)
+width  = cap.get(3)  # float `width`
+height = cap.get(4)  # float `height`
+
+# Set up output video writing
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+time_now = datetime.now()
+formatted_time = time_now.strftime("%m.%d.%Y_%H.%M.%S")
+# out = cv2.VideoWriter(f"output/pushup_{formatted_time}.mp4", fourcc, int(fps), (int(width), int(height)))
+
+detector = pm.poseDetector()
+landmarks = pl.PoseLandmark()
+count = 0
+direction = 0
+form = 0
+feedback = "Fix Form"
+
+
+while cap.isOpened():
+    ret, img = cap.read()
+
+    if img is None:
+        break
+
+    img = cv2.flip(img, 1)  # selfie view
+    
+    img = detector.findPose(img, True)
+    lmList = detector.findPosition(img, False)
+    # print(lmList)
+    if len(lmList) != 0:
+        elbow = detector.findAngle(img, landmarks.LEFT_SHOULDER, landmarks.LEFT_ELBOW, landmarks.LEFT_WRIST)
+
+        right_elbow = detector.findAngle(img, landmarks.RIGHT_SHOULDER, landmarks.RIGHT_ELBOW, landmarks.RIGHT_WRIST)
+
+        shoulder = detector.findAngle(img, 13, 11, 23)
+        hip = detector.findAngle(img, 11, 23,25)
+        
+        #Percentage of success of pushup
+        per = np.interp(elbow, (90, 160), (0, 100))
+        
+        #Bar to show Pushup progress
+        bar = np.interp(elbow, (90, 160), (380, 50))
+
+        #Check to ensure right form before starting the program
+        if elbow > 160 and shoulder > 40 and hip > 160:
+            form = 1
+    
+        #Check for full range of motion for the pushup
+        if form == 1:
+            if per == 0:
+                if elbow <= 90 and hip > 160:
+                    feedback = "Up"
+                    if direction == 0:
+                        count += 0.5
+                        direction = 1
+                else:
+                    feedback = "Fix Form"
+                    
+            if per == 100:
+                if elbow > 160 and shoulder > 40 and hip > 160:
+                    feedback = "Down"
+                    if direction == 1:
+                        count += 0.5
+                        direction = 0
+                else:
+                    feedback = "Fix Form"
+                        # form = 0
+                
+                    
+    
+        print(count)
+        
+        # TODO: draw feedback
+
+        #Draw Bar
+        # if form == 1:
+        #     cv2.rectangle(img, (580, 50), (600, 380), (0, 255, 0), 3)
+        #     cv2.rectangle(img, (580, int(bar)), (600, 380), (0, 255, 0), cv2.FILLED)
+        #     cv2.putText(img, f'{int(per)}%', (565, 430), cv2.FONT_HERSHEY_PLAIN, 2,
+        #                 (255, 0, 0), 2)
+
+
+        #Pushup counter
+        # cv2.rectangle(img, (0, 380), (100, 480), (0, 255, 0), cv2.FILLED)
+        # cv2.putText(img, str(int(count)), (25, 455), cv2.FONT_HERSHEY_PLAIN, 5,
+        #             (255, 0, 0), 5)
+        
+        #Feedback 
+        # cv2.rectangle(img, (500, 0), (640, 40), (255, 255, 255), cv2.FILLED)
+        # cv2.putText(img, feedback, (500, 40 ), cv2.FONT_HERSHEY_PLAIN, 2,
+        #             (0, 255, 0), 2)
+
+        
+    cv2.imshow('Pushup counter', img)
+    # out.write(img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+        
+cap.release()
+# out.release()
+cv2.destroyAllWindows()
